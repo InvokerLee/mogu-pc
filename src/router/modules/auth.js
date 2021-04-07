1dcdc2c91a01f6917e38c4a62d31277f0065ae7d
litianhao <none>
2021-04-07 23:43:11 +0800

    stash

--- src/router/modules/auth.js
+++ src/router/modules/auth.js
@@ -11,34 +11,34 @@ const auth = {
       path: 'user',
       name: 'user',
       component: () => import('@/views/modules/auth/user'),
-      meta: { title: '用户管理' },
+      meta: { title: '用户管理' }
     },
     {
       path: 'role',
       name: 'role',
       component: () => import('@/views/modules/auth/role'),
-      meta: { title: '角色管理' },
+      meta: { title: '角色管理' }
     },
     {
       path: 'assign-permission',
       name: 'assign-permission',
       component: () => import('@/views/modules/auth/assign-permission'),
       hidden: true,
-      meta: { title: '授权功能' },
+      meta: { title: '授权功能' }
     },
     {
       path: 'resource',
       name: 'resource',
       component: () => import('@/views/modules/auth/resource'),
-      meta: { title: '资源管理' },
+      meta: { title: '资源管理' }
     },
     {
       path: 'permission',
       name: 'permission',
       component: () => import('@/views/modules/auth/permission'),
-      meta: { title: '功能管理' },
-    },
-  ],
+      meta: { title: '功能管理' }
+    }
+  ]
 };
 
 export default auth;
